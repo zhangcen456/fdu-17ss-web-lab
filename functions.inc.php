618a15a7b9ca5e9a8fae90560af178885186f64d
zhangcen456 <2591427970@qq.com>
2018-05-17 22:48:40 +0800

    lab8

--- functions.inc.php
+++ functions.inc.php
@@ -10,6 +10,21 @@ function generateLink($url, $label, $class) {
 
 function outputPostRow($number)  {
     include("travel-data.inc.php");
+    $id1=${"postId".$number};
+    $id2=${"userId".$number};
+    $user=${"userName".$number};
+    $sdate=${"date".$number};
+    $sthumb=${"thumb".$number};
+    $stitle=${"title".$number};
+    $sexcerpt=${"excerpt".$number};
+    $sreviews=${"reviewsNum".$number};
+    $sreviewsr=${"reviewsRating".$number};
+    $imgTags=constructRating($sreviewsr);
+    $link1=generateLink("post.php?id=".$id1,'<img src="images/'.$sthumb.'" alt="'.$stitle.'" class="img-responsive"/>',"");
+    $link2=generateLink("user.php?id=".$id2,$user,"");
+    $link3=generateLink("post.php?id=".$id1,'Read more',"btn btn-primary btn-sm");
+    $strall='<div class="row"><div class="col-md-4">'.$link1.'</div><div class="col-md-8"> <h2>'.$stitle.'</h2><div class="details">Posted by '.$link2.'<span class="pull-right">'.$sdate.'</span><p class="ratings">'.$imgTags.' '.$sreviews.' Reviews</p></div><p class="excerpt">'.$sexcerpt.'</p><p>'.$link3.'</p></div></div><hr/>';
+    echo $strall;
 }
 
 /*
